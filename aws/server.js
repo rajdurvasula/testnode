deb926f96c49676320b0c69bf2eee3e4f5a6bad2
Rajasekhar Durvasula <rajasekhar.durvasul@g2w3mynywc.veregeneron.com>
2025-09-20 12:21:54 +0530

    comment out aws code. Show local hostname

--- aws/server.js
+++ aws/server.js
@@ -14,25 +14,30 @@ app.use(express.json());
 // parse request of content-type: application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended:  true }));
 
-// get EC2 Instance metadata
-const host_script = exec("curl http://169.254.169.254/latest/meta-data/hostname");
-var hostName = '';
-host_script.stdout.on('data', function(data) {
-        hostName = data;
-        //console.log(data);
-});
+// get local hostname
+const os = require('os');
+const hostName = os.hostname();
 
-var az_script = exec("curl http://169.254.169.254/latest/meta-data/placement/availability-zone");
-var azName = '';
-az_script.stdout.on('data', function(data) {
-        azName = data;
+// get EC2 Instance metadata
+//const host_script = exec("curl http://169.254.169.254/latest/meta-data/hostname");
+//var hostName = '';
+//host_script.stdout.on('data', function(data) {
+//        hostName = data;
         //console.log(data);
-});
+//});
+
+//var az_script = exec("curl http://169.254.169.254/latest/meta-data/placement/availability-zone");
+//var azName = '';
+//az_script.stdout.on('data', function(data) {
+//        azName = data;
+        //console.log(data);
+//});
 
 
 // default route
 app.get('/', (req, res) => {
-        res.json({message: `Welcome to sample node mysql restapi - from - ${hostName} - in - ${azName}`});
+//        res.json({message: `Welcome to sample node mysql restapi - from - ${hostName} - in - ${azName}`});
+        res.json({message: `Welcome to sample node mysql restapi - from - ${hostName}`});
 });
 // listen port
 const PORT = process.env.PORT || 8080;
